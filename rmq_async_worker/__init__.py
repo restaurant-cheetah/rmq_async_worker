7c9d47a63c2e52f2bcb8b472f2f7d35bfc1e648e
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-11-10 16:05:07 +0200

    Fixing init

--- rmq_async_worker/__init__.py
+++ rmq_async_worker/__init__.py
@@ -1,2 +1,3 @@
 from .async_worker import AsyncWorker, AckMode
-from .response_handler import ResponseHandler, RmqResponseHandler
+from .response_handler import ResponseHandler
+from .rmq_response_handler import RmqResponseHandler
